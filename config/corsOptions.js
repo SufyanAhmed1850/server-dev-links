3e1fec434b74ce38ed196bdfc23b18cf740dc130
Sufyan Ahmed <121061255+SufyanAhmed1850@users.noreply.github.com>
2023-12-04 14:22:58 +0500

    added netlify

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-    origin: ["http://localhost:5173", "https://devvlinks.vercel.app"],
+    origin: ["http://localhost:5173", "https://devvlinks.vercel.app", "https://devvlinks.netlify.app"],
     credentials: true,
     optionsSuccessStatus: 200,
 };
