4d84685d93dafc7c91a213136537e1c9122d3b02
Sufyan Ahmed <121061255+SufyanAhmed1850@users.noreply.github.com>
2023-12-05 17:59:49 +0500

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -10,11 +10,6 @@ const USER = express.Router();
 
 USER.get("/", getUserDetails);
 USER.post("/update-user", updateUserDetails);
-
-// Use middleware to set CORS headers for the "/image" route
-USER.post("/image", (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://devvlinks.vercel.app');
-    next();
-}, upload.single("file"), saveUserImg);
+USER.post("/image", upload.single("file"), saveUserImg);
 
 export default USER;
