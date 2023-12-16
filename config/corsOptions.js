2c4a09c52181dfc2bc7172b2b64432c6ecd72d49
SufyanAhmed1850 <sufyan.ahmed1850@gmail.com>
2023-12-16 21:53:48 +0500

    updated cors options

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,5 +1,11 @@
 const corsOptions = {
-    origin: ["http://localhost:5173", "https://devvlinks.vercel.app"],
+    origin: [
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "https://devvlinks.vercel.app",
+        "https://devvlinks.netlify.app",
+        "https://devvlinks.onrender.com",
+    ],
     credentials: true,
     optionsSuccessStatus: 200,
 };
